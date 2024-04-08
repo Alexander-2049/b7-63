f73b23ff5c7a8db86a5934ea4c4c91f2fa029f4a
Alexander2049 <a.alexander.2049@gmail.com>
2024-04-08 21:36:17 +0300

    Simple data displayment

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,20 +2,11 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import icon from '../../assets/icon.svg';
 import './App.css';
-// import { MeasureType, ValueType } from '../main/serialport';
-
-// function isValueType(value: unknown): value is ValueType {
-//   return Object.values(ValueType).includes(value as ValueType);
-// }
-
-// function isMeasureType(value: unknown): value is MeasureType {
-//   return Object.values(MeasureType).includes(value as MeasureType);
-// }
 
 function Hello() {
-  // const [value, setValue] = useState<number | null>(null);
-  // const [valueType, setValueType] = useState<ValueType | null>(null);
-  // const [measureType, setMeasureType] = useState<MeasureType | null>(null);
+  const [value, setValue] = useState<number | null>(null);
+  const [valueType, setValueType] = useState<string | null>(null);
+  const [measureType, setMeasureType] = useState<string | null>(null);
   const [connection, setConnection] = useState<boolean>(false);
 
   useEffect(() => {
@@ -26,26 +17,16 @@ function Hello() {
     window.electron.ipcRenderer.on('data-connection-status', (event) => {
       if (typeof event === 'boolean') setConnection(event);
     });
-    // window.electron.ipcRenderer.on('data-value', (event) => {
-    //   if (typeof event === 'number') setValue(event);
-    // });
-    // window.electron.ipcRenderer.on('data-measure-type', (event) => {
-    //   if (isMeasureType(event)) setMeasureType(event);
-    // });
-    // window.electron.ipcRenderer.on('data-measure-type', (event) => {
-    //   if (isValueType(event)) setValueType(event);
-    // });
+    window.electron.ipcRenderer.on('data-value', (event) => {
+      if (typeof event === 'number') setValue(event);
+    });
+    window.electron.ipcRenderer.on('data-measure-type', (event) => {
+      if (typeof event === 'string') setMeasureType(event);
+    });
+    window.electron.ipcRenderer.on('data-value-type', (event) => {
+      if (typeof event === 'string') setValueType(event);
+    });
   }, []);
-
-  // useEffect(() => {
-  //   const i = setInterval(() => {
-  //     window.electron.ipcRenderer.sendMessage('data-connection-status');
-  //   }, 1000);
-
-  //   return () => {
-  //     clearInterval(i);
-  //   };
-  // }, []);
 
   return (
     <div>
@@ -80,10 +61,10 @@ function Hello() {
         </a>
       </div>
       <div>
-        {/* <span>{value}</span>
-        <span>{valueType}</span>
-        <span>{measureType}</span> */}
-        <span>{connection ? 'Connected' : 'Disconnected'}</span>
+        <p>{value || 'No value'}</p>
+        <p>{valueType || 'No value type'}</p>
+        <p>{measureType || 'No measure type'}</p>
+        <p>{connection ? 'Connected' : 'Disconnected'}</p>
       </div>
     </div>
   );
