961bf5e891a07cda9348865dc0b28a00b7b9410e
Alexander2049 <a.alexander.2049@gmail.com>
2024-04-09 09:21:41 +0300

    Fixed context provider

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,5 @@
 import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useContext } from 'react';
 import icon from '../../assets/icon.svg';
 import './App.css';
 import useIPC from './hooks/useIPC';
@@ -6,64 +7,66 @@ import Multimeter from './pages/Multimeter';
 import IpcContext from './context/IpcContext';
 
 function Home() {
-  const ipc = useIPC();
+  const ipc = useContext(IpcContext);
 
   return (
-    <IpcContext.Provider value={ipc}>
-      <div>
-        <div className="Hello">
-          <img width="200" alt="icon" src={icon} />
-        </div>
-        <h1>electron-react-boilerplate</h1>
-        <div className="Hello">
-          <a
-            href="https://electron-react-boilerplate.js.org/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button type="button">
-              <span role="img" aria-label="books">
-                📚
-              </span>
-              Read our docs
-            </button>
-          </a>
-          <a
-            href="https://github.com/sponsors/electron-react-boilerplate"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button type="button">
-              <span role="img" aria-label="folded hands">
-                🙏
-              </span>
-              Donate
-            </button>
-          </a>
-        </div>
-        <div>
-          <p>{ipc.value || 'No value'}</p>
-          <p>{ipc.valueType || 'No value type'}</p>
-          <p>{ipc.measureType || 'No measure type'}</p>
-          <p>{ipc.connection ? 'Connected' : 'Disconnected'}</p>
-        </div>
-        <div>
-          <Link to="/multimeter">
-            <button type="button">Multimeter</button>
-          </Link>
-        </div>
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
       </div>
-    </IpcContext.Provider>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="folded hands">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+      <div>
+        <p>{ipc.value || 'No value'}</p>
+        <p>{ipc.valueType || 'No value type'}</p>
+        <p>{ipc.measureType || 'No measure type'}</p>
+        <p>{ipc.connection ? 'Connected' : 'Disconnected'}</p>
+      </div>
+      <div>
+        <Link to="/multimeter">
+          <button type="button">Multimeter</button>
+        </Link>
+      </div>
+    </div>
   );
 }
 
 export default function App() {
+  const ipc = useIPC();
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/multimeter" element={<Multimeter />} />
-      </Routes>
-    </Router>
+    <IpcContext.Provider value={ipc}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/multimeter" element={<Multimeter />} />
+        </Routes>
+      </Router>
+    </IpcContext.Provider>
   );
 }
